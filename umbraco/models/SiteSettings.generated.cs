9961d0305329ef4084803adf31611548e2d21e80
revolvermacke <max.natanaelsson@outlook.com>
2025-11-04 11:09:41 +0100

    uSync installed. Footer from siteSettings instead of hardcoded with small javascript to fix colors.

--- umbraco/models/SiteSettings.generated.cs
+++ umbraco/models/SiteSettings.generated.cs
@@ -74,6 +74,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		public virtual string ContactPhone => this.Value<string>(_publishedValueFallback, "contactPhone");
 
 		///<summary>
+		/// Footer Text
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("footerText")]
+		public virtual string FooterText => this.Value<string>(_publishedValueFallback, "footerText");
+
+		///<summary>
 		/// Platform List
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
---
+++ wwwroot/js/script.js
@@ -0,0 +1,9 @@
+﻿//Footer Copyright Highlighting
+
+document.querySelectorAll(".footerText").forEach(p => {
+    const words = p.innerHTML.trim().split(" ");
+    if (words.length > 1) {
+        const lastWord = words.pop();
+        p.innerHTML = `${words.join(" ") } <span class="highlight">${lastWord}</span>`;
+    }
+});
